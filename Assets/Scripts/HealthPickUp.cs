c0cfc400cd0b2502bc1c317e384ba1733482cb50
Moheshaa Bajpai <moheshaa13@gmail.com>
2024-02-01 18:07:29 -0800

    First Push - Mohesha

---
+++ Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//A PlayerHealth Script needs to be made in order for the below to work. I am just a 3D artist way in over my head who knows Unreal better -o-
+//public class HealthPickUp : MonoBehaviour
+//{
+  //  PlayerHealth playerHealth;
+
+   // public float healthBonus = 15f;
+
+   // void Awake()
+   // {
+    //playerHealth = FindObjectOfType<PlayerHealth>();
+   // }
+
+  //  private void OnTriggerEnter2D(Collider2D col)
+   // {
+    //    if (playerHealth.curHealth < playerHealth.maxHealth)
+   // {
+   // Destroy(gameObject);
+   // playerHealth.curHealth = playerHealth.curHealth + healthBonus;
+   // }
+   // }
+//}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    public float breath = 30f;
+
+    private bool inWater = false;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    public int health = 100;
+
+    private void Start()
+    {
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (inWater)
+        {
+            
+
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(horizontal * speed, vertical*speed);
+            breath-=.025f;
+            if (breath <= 0)
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+            Debug.Log(breath);
+
+        }
+        else
+        {
+
+            //breath = 30f;
+            horizontal = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+
+    if (Input.GetButtonDown("Jump") && IsGrounded())
+    {
+    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+    }
+
+    if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+    {
+    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+    }
+
+    Flip();
+
+    }
+
+   // private void FixedUpdate()
+    //{
+    //rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    //}
+
+    private bool IsGrounded()
+    {
+    return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+    if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+    {
+    isFacingRight = !isFacingRight;
+    Vector3 localScale = transform.localScale;
+    localScale.x *= -1f;
+    transform.localScale = localScale;
+    }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {//C
+            health = health - 5;
+            Debug.Log(health);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "water")
+        {
+            inWater = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "water")
+        {
+            inWater = false;
+            breath = 30f;
+        }
+    }
+
+
+}
