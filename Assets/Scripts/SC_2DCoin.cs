1f8414ae782be09fda0932aa33989e2fcf439c53
Moheshaa Bajpai <moheshaa13@gmail.com>
2024-02-01 23:31:55 -0800

    game over after health

--- Assets/Scripts/SC_2DCoin.cs
+++ Assets/Scripts/SC_2DCoin.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SC_2DCoin : MonoBehaviour
 {
     //Keep track of total picked coins (Since the value is static, it can be accessed at "SC_2DCoin.totalCoins" from any script)
     public static int totalCoins = 0;
 
-    public static int PlayerHealth = 0;
+    public static int PlayerHealth = 10;
 
 
     void Awake()
@@ -16,6 +17,14 @@ public class SC_2DCoin : MonoBehaviour
         //Make Collider2D as trigger 
         GetComponent<Collider2D>().isTrigger = true;
 
+    }
+
+    private void Update()
+    {
+        if (PlayerHealth <= 0)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
 
 
@@ -27,9 +36,6 @@ public class SC_2DCoin : MonoBehaviour
             //Add coin to counter
             totalCoins++;
             PlayerHealth = PlayerHealth + 5;
-            //Test: Print total number of coins
-            //Debug.Log("You currently have " + SC_2DCoin.totalCoins + " Coins.");
-            //Destroy coin
             Destroy(gameObject);
         }
     }
